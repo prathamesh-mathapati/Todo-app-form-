ef35331f187dc8d5fe97e548df2b05e5f9c6a3f9
Prathamesh Mathapati <93044026+prathamesh-mathapati@users.noreply.github.com>
2022-05-09 11:12:30 +0530

    Add files via upload

--- stript.js
+++ stript.js
@@ -29,7 +29,7 @@ const htmlData = (user) => `<div>
 
         <span class="d-flex">
           Stroge:-
-          <p class="roll">${user.storeg}</p></span
+          <p class="roll">${user.storage}</p></span
         >
 
          <form class="">
@@ -41,43 +41,50 @@ const htmlData = (user) => `<div>
       </div>
 
      `;
-// console.log(roll.value);
+let localDatastring1 = localStorage.getItem("data");
+var arrForlocaldata = [];
+
+if (localDatastring1.name === "") {
+  console.log("as");
+} else {
+  arrForlocaldata.push(JSON.parse(localDatastring1));
+  for (i in arrForlocaldata.flat(Infinity)) {
+    AllData.push(arrForlocaldata.flat(Infinity)[i]);
+    console.log(AllData);
+  }
+}
+
 submitBtn.addEventListener("click", () => {
   let oneDta = {};
 
   oneDta["name"] = name.value;
   oneDta["Subject"] = nameInput.value;
   oneDta["Roll_no"] = roll.value;
-  oneDta["storeg"] = storage.selectedOptions[0].innerText;
+  oneDta["storage"] = storage.selectedOptions[0].innerText;
 
   // AllData.push(JSON.stringify(oneDta));
   AllData.push(oneDta);
 
   AllData.map((ele) => {
+    // console.log(ele);
     if (ele.storage === "Local Storage") {
       if (!ele.Roll_no) {
         alert("Please fill full from");
       } else {
-        let localDatastring1 = localStorage.getItem("data");
-        console.log(localDatastring1);
         window.localStorage.setItem("data", JSON.stringify(AllData));
         let localDatastring = localStorage.getItem("data");
-        console.log(localDatastring);
         let localData = JSON.parse(localDatastring);
-        console.log(typeof localData);
         Data.innerHTML = AllData.map(htmlData);
         Data.classList.remove("hid");
       }
     } else {
-      let localDatastring1 = sessionStorage.getItem("data");
-      console.log(localDatastring1);
-      window.sessionStorage.setItem("data", JSON.stringify(AllData));
-      let localDatastring = sessionStorage.getItem("data");
-      console.log(localDatastring);
-      let localData = JSON.parse(localDatastring);
-      console.log(typeof localData);
-      Data.innerHTML = AllData.map(htmlData);
-      Data.classList.remove("hid");
+      // window.sessionStorage.setItem("data", JSON.stringify(AllData));
+      // let localDatastring = sessionStorage.getItem("data");
+      // console.log(localDatastring);
+      // let localData = JSON.parse(localDatastring);
+      // console.log(typeof localData);
+      // Data.innerHTML = AllData.map(htmlData);
+      // Data.classList.remove("hid");
     }
   });
 });
